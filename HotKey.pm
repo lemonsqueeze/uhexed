7eb1015fd5dc8cb86fa53fb2af6acebea617f0b5
lemonsqueeze <lemonsqueeze@gmx.com>
2016-06-02 16:16:40 +0200

    help

--- HotKey.pm
+++ HotKey.pm
@@ -1,41 +1,43 @@
 # HotKey.pm
-    package HotKey;
+# From man perlfaq8
 
-    @ISA = qw(Exporter);
-    @EXPORT = qw(cbreak cooked readkey);
+package HotKey;
 
-    use strict;
-    use POSIX qw(:termios_h);
-    my ($term, $oterm, $echo, $noecho, $fd_stdin);
+@ISA = qw(Exporter);
+@EXPORT = qw(cbreak cooked readkey);
 
-    $fd_stdin = fileno(STDIN);
-    $term     = POSIX::Termios->new();
-    $term->getattr($fd_stdin);
-    $oterm     = $term->getlflag();
+use strict;
+use POSIX qw(:termios_h);
+my ($term, $oterm, $echo, $noecho, $fd_stdin);
 
-    $echo     = ECHO | ECHOK | ICANON;
-    $noecho   = $oterm & ~$echo;
+$fd_stdin = fileno(STDIN);
+$term     = POSIX::Termios->new();
+$term->getattr($fd_stdin);
+$oterm     = $term->getlflag();
 
-    sub cbreak {
-        $term->setlflag($noecho);  # ok, so i don't want echo either
-        $term->setcc(VTIME, 1);
-        $term->setattr($fd_stdin, TCSANOW);
-    }
+$echo     = ECHO | ECHOK | ICANON;
+$noecho   = $oterm & ~$echo;
 
-    sub cooked {
-        $term->setlflag($oterm);
-        $term->setcc(VTIME, 0);
-        $term->setattr($fd_stdin, TCSANOW);
-    }
+sub cbreak {
+    $term->setlflag($noecho);  # ok, so i don't want echo either
+    $term->setcc(VTIME, 1);
+    $term->setattr($fd_stdin, TCSANOW);
+}
 
-    sub readkey {
-        my $key = '';
-        cbreak();
-        sysread(STDIN, $key, 1);
-        cooked();
-        return $key;
-    }
+sub cooked {
+    $term->setlflag($oterm);
+    $term->setcc(VTIME, 0);
+    $term->setattr($fd_stdin, TCSANOW);
+}
 
-    END { cooked() }
+sub readkey {
+    my $key = '';
+    cbreak();
+    sysread(STDIN, $key, 1);
+    cooked();
+    return $key;
+}
 
-    1;
+END { cooked() }
+
+1;
